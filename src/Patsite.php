6e94ec6bd9629adb0026064b7eed490819960b0f
Patryk <patryk@frontendfoundry.dev>
2025-07-15 23:21:37 +0100

    Update Patsite.php

--- src/Patsite.php
+++ src/Patsite.php
@@ -5,27 +5,36 @@ use PatrykNamyslak\Patbase as Database;
 
 
 class Patsite{
+    protected Database $db;
+
+    protected function __construct(?string $db_name = NULL, ?string $db_username = NULL, ?string $db_password = NULL){
+        $this->db = new Database(
+            database_name: $db_name ?? $_ENV['PATSITE_DATABASE_NAME'] ?? 'clients',
+            username: $db_username ?? $_ENV['PATSITE_DATABASE_USERNAME'],
+            password: $db_password ?? $_ENV['PATSITE_DATABASE_PASSWORD']
+        );
+    }
+
     final public static function website(string $ID){
-        return new Website($ID);
+        $Patsite = new self;
+        return new Website(ID: $ID, db_connection: $Patsite->db);
     }
 }
 
+
+
+
 final class Website extends Patsite{
     private string $ID;
-    private Database $db;
 
     /**
      * @param string|null $db_username : This gives you a choice if you want to manually enter the username or just use the environmental variables in the env file
      * @param string|null $db_password : This gives you a choice if you want to manually enter the password or just use the environmental variables in the env file
      * @param string|null $db_name : This gives you a choice if you want to manually enter the database name or just use the environmental variables in the env file or just the default clients name
      */
-    protected function __construct($ID, ?string $db_name = NULL, ?string $db_username = NULL, ?string $db_password = NULL){
+    protected function __construct(string $ID, Database $db_connection){
         $this->ID = $ID;
-        $this->db = new Database(
-            database_name: $db_name ?? $_ENV['PATSITE_DATABASE_NAME'] ?? 'clients',
-            username: $db_username ?? $_ENV['PATSITE_DATABASE_USERNAME'],
-            password: $db_password ?? $_ENV['PATSITE_DATABASE_PASSWORD']
-        );
+        $this->db = $db_connection;
     }
     /**
      * This function is used to fetch website data.
